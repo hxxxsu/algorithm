2c03e011122bb1311e9da9e621820671c0c7321f
정현수 <jsusu7993@gmail.com>
2021-09-24 00:03:29 +0900

    [21/09/23] 알파벳찾기 개선

--- baekjoon/7단계_문자열/10809_알파벳찾기.js
+++ baekjoon/7단계_문자열/10809_알파벳찾기.js
@@ -2,14 +2,17 @@ const input = require('fs').readFileSync(
   'baekjoon/testcase/7/10809.txt',
   'utf-8'
 );
-
-console.log(input);
-
 let arr = input.split('').map(x => x.charCodeAt());
-
 let text = [];
 for (let i = 97; i <= 122; i++) {
   text.push(arr.indexOf(i));
 }
+console.log(text.join(' '));
 
-console.log(text);
+//숏코딩
+const input = require('fs').readFileSync('/dev/stdin').toString();
+let result = [];
+for (let i = 97; i <= 122; i++) {
+  result.push(input.indexOf(String.fromCharCode(i)));
+}
+console.log(result.join(' '));
