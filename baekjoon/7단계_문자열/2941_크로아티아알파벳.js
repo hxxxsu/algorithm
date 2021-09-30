4793bc9fcbc579f3420bdee125b0f31fb869af11
정현수 <jsusu7993@gmail.com>
2021-10-01 01:14:20 +0900

    [21/09/30] 크로아티아 개선

--- baekjoon/7단계_문자열/2941_크로아티아알파벳.js
+++ baekjoon/7단계_문자열/2941_크로아티아알파벳.js
@@ -1,15 +1,27 @@
 let s = require('fs').readFileSync('baekjoon/testcase/7/2941.txt', 'utf8').trim();
 let arr = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z='];
 
-console.log(s, arr);
-
+let cnt = 0;
 while (s.length > 0) {
-  console.log('=========');
+  let isOne = true;
   arr.forEach(data => {
-    console.log('@@@@@@@@@@');
     if (s.startsWith(data)) {
       s = s.substr(data.length, s.length);
-      console.log(s);
+      isOne = false;
+      cnt++;
+      return;
     }
   });
+  if (isOne) {
+    s = s.substr(1, s.length);
+    cnt++;
+  }
 }
+
+console.log(cnt);
+
+//숏코딩
+const s = require('fs').readFileSync('baekjoon/testcase/7/2941.txt', 'utf8').trim();
+let a = s.replace(/c\=|c\-|dz\=|d\-|lj|nj|s\=|z\=/g, ' ');
+
+console.log(a, a.length);
