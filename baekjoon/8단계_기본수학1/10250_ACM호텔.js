3e14f9a4de3f74e2fb0ac0208eb99c86703b908e
정현수 <jsusu7993@gmail.com>
2021-10-14 00:37:50 +0900

    [21/10/14]

--- baekjoon/8단계_기본수학1/10250_ACM호텔.js
+++ baekjoon/8단계_기본수학1/10250_ACM호텔.js
@@ -1,6 +1,21 @@
-let s = require('fs').readFileSync('baekjoon/testcase/8/10250.txt', 'utf8').split('\n');
-console.log(s);
-for (let i = 1; i <= s[0]; i++) {
-  const [H, W, N] = s[i].split(' ').map(Number);
-  console.log(H, W, N);
+// let s = require('fs').readFileSync('baekjoon/testcase/8/10250.txt', 'utf8').split('\n');
+// for (let i = 1; i <= s[0]; i++) {
+//   let arr = [];
+//   const [H, W, N] = s[i].split(' ').map(Number);
+//   for (let j = 1; j <= W; j++) {
+//     for (let k = 1; k <= H; k++) {
+//       arr.push(k * 100 + j);
+//     }
+//   }
+//   console.log(arr[N - 1]);
+// }
+
+let s = require('fs')
+  .readFileSync('baekjoon/testcase/8/10250.txt', 'utf-8')
+  .trim()
+  .split('\n')
+  .slice(1);
+for (let d of s) {
+  const [h, w, n] = d.split(' ').map(Number);
+  console.log(`${n % h === 0 ? h : n % h}${String(Math.ceil(n / h)).padStart(2, '0')}`);
 }
