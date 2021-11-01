062095af69f900135358ef9d225511ae393389e2
정현수 <jsusu7993@gmail.com>
2021-11-01 23:36:06 +0900

    [21/11/01] 네번째점 개선

--- baekjoon/9단계_기본수학2/3009_네번째점.js
+++ baekjoon/9단계_기본수학2/3009_네번째점.js
@@ -1,8 +1,29 @@
 let input = require('fs').readFileSync('baekjoon/testcase/9/3009.txt', 'utf8').split('\n');
-let xArr = [];
-let yArr = [];
-for (let str of input) {
+let xObj = {},
+  yObj = {};
+for (const str of input) {
   let [x, y] = str.split(' ');
-  xArr.push(x);
-  yArr.push(y);
+  xObj[x] ? xObj[x]++ : (xObj[x] = 1);
+  yObj[y] ? yObj[y]++ : (yObj[y] = 1);
 }
+console.log(
+  Object.keys(xObj).find(key => xObj[key] === 1),
+  Object.keys(yObj).find(key => yObj[key] === 1)
+);
+
+// 코드개선
+
+let X = 0,
+  Y = 0;
+
+require('fs')
+  .readFileSync('baekjoon/testcase/9/3009.txt', 'utf8')
+  .split('\n')
+  .forEach(line => {
+    const [x, y] = line.split(' ').map(Number);
+    X ^= x;
+    Y ^= y;
+    console.log(X, Y);
+  });
+
+console.log(X, Y);
