807914110912c7c5589a914e84bd2d3c7a27cecc
jsusu7993 <jsusu7993@gmail.com>
2021-11-04 23:43:35 +0900

    test

---
+++ baekjoon/10단계_재귀/10872_factorial.js
@@ -0,0 +1,14 @@
+// factorial(=팩토리얼,팩,계승) 그 수보다 작거나 같은 모든 양의 정수의 곱 이다
+// 즉, 3! = 1*2*3 = 6
+
+// const input = +require('fs').readFileSync(0, 'utf8');
+
+// const input = 2;
+
+// console.log(
+//   Array(...Array(input).keys())
+//     .map(x => x + 1)
+//     .reduce((a, b) => a * b)
+// );
+
+console.log(Array(...Array(1).keys()).map(x => x + 1));
