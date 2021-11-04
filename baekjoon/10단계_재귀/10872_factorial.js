35337d329f73dcf1314e2678a2b5421542be11df
정현수 <jsusu7993@gmail.com>
2021-11-04 23:58:40 +0900

    [21/11/04] 팩토리얼

--- baekjoon/10단계_재귀/10872_factorial.js
+++ baekjoon/10단계_재귀/10872_factorial.js
@@ -1,14 +1,24 @@
-// factorial(=팩토리얼,팩,계승) 그 수보다 작거나 같은 모든 양의 정수의 곱 이다
-// 즉, 3! = 1*2*3 = 6
+/** factorial(=팩토리얼,팩,계승) 그 수보다 작거나 같은 모든 양의 정수의 곱 이다
+즉, 3! = 1*2*3 = 6 */
 
 // const input = +require('fs').readFileSync(0, 'utf8');
-
 // const input = 2;
-
+// if (input < 2) {
+//   console.log(1);
+//   return;
+// }
 // console.log(
 //   Array(...Array(input).keys())
 //     .map(x => x + 1)
 //     .reduce((a, b) => a * b)
 // );
 
-console.log(Array(...Array(1).keys()).map(x => x + 1));
+// 코드개선 (재귀)
+let a = 10;
+
+function fact(n) {
+  if (n < 2) return 1;
+  return n * fact(n - 1);
+}
+
+console.log(fact(a));
