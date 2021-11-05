93f96ccadcb24f09921c78105745acf949298127
정현수 <jsusu7993@gmail.com>
2021-11-05 23:48:18 +0900

    [21/11/05] 피보나치 5

---
+++ baekjoon/10단계_재귀/10870_피보나치.js
@@ -0,0 +1,23 @@
+// let a = +require('fs').readFileSync(0, 'utf8');
+let input = 10;
+
+let arr = [];
+
+for (let i = 0; i <= input; i++) {
+  if (i < 2) {
+    arr.push(i);
+    continue;
+  }
+
+  arr.push(arr[i - 1] + arr[i - 2]);
+}
+
+console.log(arr[arr.length - 1]);
+
+///////////////////
+const fibo = n => {
+  let test = n <= 1 ? n : fibo(n - 1) + fibo(n - 2);
+  console.log(n, 'test', test);
+  return test;
+};
+console.log(fibo(input));
