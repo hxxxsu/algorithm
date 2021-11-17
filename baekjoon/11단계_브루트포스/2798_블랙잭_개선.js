a40d86808412e2ffae4aab1ac59d82e1e0028178
정현수 <jsusu7993@gmail.com>
2021-11-17 23:57:54 +0900

    [21/11/17] 블랙잭 개선2

--- baekjoon/11단계_브루트포스/2798_블랙잭_개선.js
+++ baekjoon/11단계_브루트포스/2798_블랙잭_개선.js
@@ -1,16 +1,21 @@
+// const input = ['10 500', '93 181 245 214 315 36 185 138 216 295'];
 const input = ['5 21', '5 6 7 8 9'];
-const M = [];
-let arr = input
-  .split(' ')
-  .map(Number)
-  .sort((a, b) => b - a);
-console.log(arr);
+const NM = input.shift().split(' ').map(Number);
+const N = NM.shift();
+const M = NM.shift();
+const cards = input.shift().split(' ').map(Number);
 
-let sum = 0;
+let max = 0;
 
-let answer = [0, 1, 2];
-
-while (M != sum) {
-  sum = arr[answer[0]] + arr[answer[1]] + arr[answer[2]];
-  i;
+for (let i = 0; i < N - 2; i++) {
+  // 총카드 갯수 - 1 번 반복하는 for문
+  console.log('====1====', i);
+  for (let j = i + 1; j < N - 1; j++) {
+    console.log('====2====', j);
+    for (let k = j + 1; k < N; k++) {
+      // 총카드 갯수 만큼 반복하는 for문
+      console.log('====3====', k);
+      console.log(cards[i], cards[j], cards[k], '=', cards[i] + cards[j] + cards[k]);
+    }
+  }
 }
