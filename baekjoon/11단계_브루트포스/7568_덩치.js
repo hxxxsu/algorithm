66fd82f64b6d4f59e2ba332356846adc8415d6a4
정현수 <jsusu7993@gmail.com>
2021-11-30 23:47:26 +0900

    [21/11/30] 덩치 개선

--- baekjoon/11단계_브루트포스/7568_덩치.js
+++ baekjoon/11단계_브루트포스/7568_덩치.js
@@ -1,13 +1,22 @@
-const input = ['5', '55 185', '58 183', '88 186', '60 175', '46 155'];
-let N = input.shift();
-console.log(input, N);
+// const input = require('fs').readFileSync(0, 'utf8').split('\n');
+// const input = ['5', '55 185', '58 183', '88 186', '60 175', '46 155'];
+// let N = input.shift();
+// let text = [];
+// for (let i = 0; i < N; i++) {
+//   let [x, y] = input[i].split(' ').map(Number);
+//   let rank = 1;
+//   for (let j = 0; j < N; j++) {
+//     if (j == i) continue;
+//     let [p, q] = input[j].split(' ').map(Number);
+//     if (x < p && y < q) rank++;
+//   }
+//   text.push(rank);
+// }
+// console.log(text.join(' '));
 
-for (let i = 0; i < N; i++) {
-  console.log('==========');
-  let me = input[i].split(' ').map(Number);
-  for (let j = 0; j < N; j++) {
-    if (j == i) continue;
-    let other = input[j];
-    console.log(me, other);
-  }
-}
+// 개선
+const input = ['5', '55 185', '58 183', '88 186', '60 175', '46 155']
+  .filter((e, i) => i > 0)
+  .map(e => e.split(' ').map(v => Number(v)));
+const result = input.map(e => input.filter(v => v[0] > e[0] && v[1] > e[1]).length + 1);
+console.log(result.join());
