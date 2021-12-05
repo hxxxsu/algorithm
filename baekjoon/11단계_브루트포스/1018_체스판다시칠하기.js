9e69ea2eebf61c7217e0a3c581695777c66cf981
정현수 <jsusu7993@gmail.com>
2021-12-05 23:58:53 +0900

    [21/12/05] 체스판다시칠하기 --2

--- baekjoon/11단계_브루트포스/1018_체스판다시칠하기.js
+++ baekjoon/11단계_브루트포스/1018_체스판다시칠하기.js
@@ -1,3 +1,7 @@
 const input = require('fs').readFileSync('baekjoon/testcase/11/1018.txt', 'utf8').split('\n');
 const [N, M] = input.shift().split(' ').map(Number);
 console.log(N, M, input);
+
+for (let i = 0; i < N; i++) {
+  console.log(input[i]);
+}
