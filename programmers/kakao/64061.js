208ae77d0fb41e517cdfaafe8275287db78f1919
정현수 <jsusu7993@gmail.com>
2021-12-26 23:22:40 +0900

    [21/12/26] 인형뽑기 개선

--- programmers/kakao/64061.js
+++ programmers/kakao/64061.js
@@ -2,35 +2,34 @@
 function solution(board, moves) {
   let bd = [...board];
   let arr = [];
+  let cnt = 0;
 
   moves
     .map(move => move - 1)
     .forEach(move => {
       const idx = bd.findIndex(item => item[move] > 0);
-
       if (idx != -1) {
-        arr.push(board[idx][move]);
+        if (arr[arr.length - 1] === board[idx][move]) {
+          arr.pop();
+          cnt++;
+        } else {
+          arr.push(board[idx][move]);
+        }
         board[idx][move] = 0;
       }
     });
-  console.log('board', board);
-  console.log('arr', arr);
-
-  arr.forEach((item, i) => {
-    if (arr.length === i - 1) return;
-    if (item === arr[i + 1]) (arr[i] = 0), (arr[i + 1] = 0);
-  });
-
-  console.log('arr', arr);
+  return cnt * 2;
 }
 
-solution(
-  [
-    [0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 3],
-    [0, 2, 5, 0, 1],
-    [4, 2, 4, 4, 2],
-    [3, 5, 1, 3, 1]
-  ],
-  [1, 5, 3, 5, 1, 2, 1, 4]
+console.log(
+  solution(
+    [
+      [0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 3],
+      [0, 2, 5, 0, 1],
+      [4, 2, 4, 4, 2],
+      [3, 5, 1, 3, 1]
+    ],
+    [1, 5, 3, 5, 1, 2, 1, 4]
+  )
 );
