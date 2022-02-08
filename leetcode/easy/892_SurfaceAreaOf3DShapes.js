d37caf1b332d56ed0919e34110039322c32c190d
정현수 <jsusu7993@gmail.com>
2022-02-08 22:34:12 +0900

    [22/02/08] leetcode easy 999 진행중

--- leetcode/easy/892_SurfaceAreaOf3DShapes.js
+++ leetcode/easy/892_SurfaceAreaOf3DShapes.js
@@ -7,7 +7,7 @@ var surfaceArea = function (grid) {
   let res = 0;
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-      let current = gird[i][j];
+      let current = grid[i][j];
       // 기둥 단위로 계산하기 위해 기둥 높이에 4를 곱하여 4면과 위 아래 2를 더하여 기둥의 총 면을 구함
       if (current !== 0) res += current * 4 + 2;
       // 기둥의 총 면에서 앞 뒤 위 아래로 겹쳐지는 면이 있는 경우 제거함.
