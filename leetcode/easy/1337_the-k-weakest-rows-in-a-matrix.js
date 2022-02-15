83a4b08f06869239d7df39a22ecda3464bfcd8c1
정현수 <jsusu7993@gmail.com>
2022-02-15 21:33:02 +0900

    [22/02/15] leetcode easy 1337

---
+++ leetcode/easy/1337_the-k-weakest-rows-in-a-matrix.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number[]}
+ */
+var kWeakestRows = function (mat, k) {
+  return mat
+    .map((item, i) => [i, item.reduce((a, b) => a + b)])
+    .sort((a, b) => a[1] - b[1])
+    .slice(0, k)
+    .map(item => item[0]);
+};
+
+console.log(
+  kWeakestRows(
+    [
+      [1, 1, 0, 0, 0],
+      [1, 1, 1, 1, 0],
+      [1, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0],
+      [1, 1, 1, 1, 1]
+    ],
+    3
+  )
+);
+
+console.log(
+  kWeakestRows(
+    [
+      [1, 0, 0, 0],
+      [1, 1, 1, 1],
+      [1, 0, 0, 0],
+      [1, 0, 0, 0]
+    ],
+    2
+  )
+);
