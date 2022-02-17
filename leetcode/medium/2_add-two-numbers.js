318e5ddab46b5db4414dcb3c7507491dfedf74d5
정현수 <jsusu7993@gmail.com>
2022-02-17 21:04:13 +0900

    [22/02/17] leetcode 2 add-two-numbers imporve

--- leetcode/medium/2_add-two-numbers.js
+++ leetcode/medium/2_add-two-numbers.js
@@ -1,3 +1,29 @@
+var addTwoNumbers = function (l1, l2) {
+  var List = new L(0);
+  var head = List;
+  var sum = 0;
+  var carry = 0;
+  while (l1 !== null || l2 !== null || sum > 0) {
+    if (l1 !== null) {
+      sum = sum + l1.val;
+      l1 = l1.next;
+    }
+    if (l2 !== null) {
+      sum = sum + l2.val;
+      l2 = l2.next;
+    }
+    if (sum >= 10) {
+      carry = 1;
+      sum = sum - 10;
+    }
+    head.next = new L(sum);
+    head = head.next;
+    sum = carry;
+    carry = 0;
+  }
+  return List.next;
+};
+
 function L(val, next) {
   this.val = val === undefined ? 0 : val;
   this.next = next === undefined ? null : next;
@@ -15,8 +41,16 @@ function aToL(a) {
   }
   return n;
 }
-var addTwoNumbers = function (l1, l2) {
-  console.log(l1, l2);
+
+const logL = l => {
+  let _l = l;
+  let arr = [];
+  while (_l !== null) {
+    arr.push(_l.val);
+    _l = _l.next;
+  }
+  console.log(arr.join(''));
 };
 
-console.log(addTwoNumbers(aToL([2, 4, 3]), aToL([5, 6, 4])));
+// console.log(addTwoNumbers(aToL([2, 4, 3]), aToL([5, 6, 4])));
+logL(addTwoNumbers(aToL([9, 9, 9, 9, 9, 9, 9]), aToL([9, 9, 9, 9])));
